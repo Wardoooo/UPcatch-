843ad81cc028c0f2ef7b0ee3c4d46e4b5d64f02d
Wardoooo <jofloresferrer@gmail.com>
2026-06-07 00:27:44 +0800

    credit page improved + skip intro and tutorial + UPLOGO real quality non pixelated

--- INTRO.cpp
+++ INTRO.cpp
@@ -2,6 +2,7 @@
 #include <Godot/godot.hpp>
 #include <Godot/classes/video_stream_player.hpp>
 #include <Godot/classes/scene_tree.hpp>
+#include <Godot/classes/input.hpp>
 
 using namespace godot;
 using namespace jenova::sdk;
@@ -28,15 +29,38 @@ void OnReady(Caller* instance)
 		self->play();
 }
 
+void GoToMenu()
+{
+	if (!self || changed_scene)
+		return;
+
+	changed_scene = true;
+	self->stop();
+	self->get_tree()->change_scene_to_file("res://menu.tscn");
+}
+
 void OnProcess(Caller* instance, double delta)
 {
 	if (!self || changed_scene)
 		return;
 
+	Input* input = Input::get_singleton();
+
+	if (
+		input->is_key_pressed(Key::KEY_SPACE) ||
+		input->is_key_pressed(Key::KEY_ENTER) ||
+		input->is_key_pressed(Key::KEY_ESCAPE) ||
+		input->is_anything_pressed()
+	)
+	{
+		GoToMenu();
+		return;
+	}
+
+
 	if (!self->is_playing())
 	{
-		changed_scene = true;
-		self->get_tree()->change_scene_to_file("res://menu.tscn");
+		GoToMenu();
 	}
 }
 
--- TUTORIALTOMENU.cpp
+++ TUTORIALTOMENU.cpp
@@ -2,6 +2,7 @@
 #include <Godot/godot.hpp>
 #include <Godot/classes/video_stream_player.hpp>
 #include <Godot/classes/scene_tree.hpp>
+#include <Godot/classes/input.hpp>
 
 using namespace godot;
 using namespace jenova::sdk;
@@ -28,15 +29,32 @@ void OnReady(Caller* instance)
 		TUTVID->play();
 }
 
+void GoToMenu()
+{
+	if (!TUTVID || changed_scene)
+		return;
+
+	changed_scene = true;
+	TUTVID->stop();
+	TUTVID->get_tree()->change_scene_to_file("res://menu.tscn");
+}
+
 void OnProcess(Caller* instance, double delta)
 {
 	if (!TUTVID || changed_scene)
 		return;
 
+
+	if (Input::get_singleton()->is_key_pressed(Key::KEY_SPACE))
+	{
+		GoToMenu();
+		return;
+	}
+
+	
 	if (TUTVID->get_stream_position() >= 74.0)
 	{
-		changed_scene = true;
-		TUTVID->get_tree()->change_scene_to_file("res://menu.tscn");
+		GoToMenu();
 	}
 }
 
